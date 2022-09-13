fff91554b76a3fb18e259171b285d677c73a4873
indigopurple <indigopurple@163.com>
2022-09-13 21:11:18 +0800

    first update

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
 # PATE
-PATE: Property, Amenities, Traffic and Emotions Coming Together for Real Estate Price Prediction
+In this repository we provide code of the paper:
+> **PATE: Property, Amenities, Traffic and Emotions Coming Together for Real Estate Price Prediction**
+
+> Yaping Zhao, Ramgopal Ravi, Shuhui Shi, Zhongrui Wang, Edmund Y. Lam, Jichang Zhao
+
+> arxiv link: https://arxiv.org/abs/2209.05471
+
+The **H4M Dataset** is released at: [https://indigopurple.github.io/H4M/index.html](https://indigopurple.github.io/H4M/index.html)
+
+<p align="left">
+<img src="corr.png">
+</p>
+
+# Usage
+0. For pre-requisites, run:
+```
+conda env create -f environment.yml
+conda activate pate
+```
+1. To reproduce the results and figures in the paper, run:
+```
+python main.py
+```
+2. For further research, visit the website of [H4M Dataset](https://indigopurple.github.io/H4M/index.html).
+
+# Citation
+Cite our paper if you find it interesting!
+```
+@misc{zhao2022pate,
+      title={PATE: Property, Amenities, Traffic and Emotions Coming Together for Real Estate Price Prediction}, 
+      author={Zhao, Yaping and Ravi, Ramgopal and Shi, Shuhui and Wang, Zhongrui and Lam, Edmund Y. and Zhao, Jichang},
+      journal={arXiv preprint arXiv:2209.05471},
+      year={2022}
+}
+
+@article{zhao2022h4m,
+  title={H4M: Heterogeneous, Multi-source, Multi-modal, Multi-view and Multi-distributional Dataset for Socioeconomic Analytics in the Case of Beijing},
+  author={Zhao, Yaping and Shi, Shuhui and Ravi, Ramgopal and Wang, Zhongrui and Lam, Edmund Y and Zhao, Jichang},
+  journal={arXiv preprint arXiv:2208.12542},
+  year={2022}
+}
+```
---
+++ main.py
@@ -0,0 +1,200 @@
+# Importing the libraries
+import pandas as pd
+import numpy as np
+from sklearn import metrics
+import matplotlib.pyplot as plt
+import seaborn as sns
+# %matplotlib inline
+import csv
+
+from sklearn.datasets import load_boston
+
+# Initializing the data
+csv_file = open('/home/yaping/projects/H4M/dsaa_dataset_order_rename.csv')
+rows = csv.reader(csv_file)
+rows = list(rows)
+data = pd.DataFrame(rows[1:]).astype(float)
+
+# preprocessing and check the data
+print(data.shape)
+data.columns = rows[0]
+data = data.drop('id', axis=1)
+data = data.drop('TtlPrc', axis=1)
+data = data.rename(columns={"UntPrc": "Price"})
+
+use_property_only = 0
+remove_a, remove_t, remove_e = 0,0,0
+if use_property_only:
+    data = data.drop(['TspNum','TspDst', 'AtrNum','AtrDst','EdcNum','EdcDst','HthNum',
+                      'HthDst','RstNum','RstDst','RtlNum','RtlDst','TrfV',
+                      'AgrPct','DstPct','HppPct','SadPct','FeaPct'], axis=1)
+else:
+    if remove_a:
+        data = data.drop(['TspNum', 'TspDst', 'AtrNum', 'AtrDst', 'EdcNum', 'EdcDst', 'HthNum',
+                          'HthDst', 'RstNum', 'RstDst', 'RtlNum', 'RtlDst'], axis=1)
+    if remove_t:
+        data = data.drop(['TrfV'], axis=1)
+    if remove_e:
+        data = data.drop(['AgrPct', 'DstPct', 'HppPct', 'SadPct', 'FeaPct'], axis=1)
+
+
+print(data.head())
+print(data.dtypes)
+print(data.isnull().sum())
+
+# Viewing the data statistics
+print(data.describe())
+
+# Finding out the correlation between the features
+corr = data.corr()
+print(corr.shape)
+
+# Plotting the heatmap of correlation between features
+plt.figure(figsize=(20,20))
+sns.set(font_scale=1.5)
+sns.heatmap(corr, cbar=True, square= True, fmt='.1f', annot=True, annot_kws={'size':15}, cmap='Greens')
+plt.show()
+
+# Spliting target variable and independent variables
+X = data.drop(['Price'], axis = 1)
+y = data['Price']
+
+# Splitting to training and testing data
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X,y, test_size = 0.3, random_state = 4)
+
+print('------------Linear Regression-----------------')
+# Import library for Linear Regression
+from sklearn.linear_model import LinearRegression
+
+# Create a Linear regressor
+lm = LinearRegression()
+
+# Train the model using the training sets
+lm.fit(X_train, y_train)
+
+LinearRegression(copy_X=True, fit_intercept=True, n_jobs=None,
+         normalize=False)
+
+# Value of y intercept
+print(lm.intercept_)
+
+#Converting the coefficient values to a dataframe
+coeffcients = pd.DataFrame([X_train.columns,lm.coef_]).T
+coeffcients = coeffcients.rename(columns={0: 'Attribute', 1: 'Coefficients'})
+print(coeffcients)
+
+# Model prediction on train data
+y_pred = lm.predict(X_train)
+# Model Evaluation
+print('R^2:',metrics.r2_score(y_train, y_pred))
+print('Adjusted R^2:',1 - (1-metrics.r2_score(y_train, y_pred))*(len(y_train)-1)/(len(y_train)-X_train.shape[1]-1))
+print('MAE:',metrics.mean_absolute_error(y_train, y_pred))
+print('MSE:',metrics.mean_squared_error(y_train, y_pred))
+print('RMSE:',np.sqrt(metrics.mean_squared_error(y_train, y_pred)))
+
+# Predicting Test data with the model
+y_test_pred = lm.predict(X_test)
+# Model Evaluation
+acc_linreg = metrics.r2_score(y_test, y_test_pred)
+mae_linreg = metrics.mean_absolute_error(y_test, y_test_pred)
+rmse_linreg = np.sqrt(metrics.mean_squared_error(y_test, y_test_pred))
+print('R^2:', acc_linreg)
+print('Adjusted R^2:',1 - (1-metrics.r2_score(y_test, y_test_pred))*(len(y_test)-1)/(len(y_test)-X_test.shape[1]-1))
+print('MAE:',mae_linreg)
+print('MSE:',metrics.mean_squared_error(y_test, y_test_pred))
+print('RMSE:',rmse_linreg)
+
+# Visualizing the differences between actual prices and predicted values
+plt.clf()
+plt.scatter(y_test, y_test_pred)
+plt.xlabel("Prices")
+plt.ylabel("Predicted prices", labelpad=1.5)
+plt.title("Prices vs Predicted prices")
+xpoints = ypoints = plt.xlim()
+plt.plot(xpoints, ypoints, linestyle='-', color='r', lw=3, scalex=False, scaley=False)
+plt.show()
+
+# Checking residuals
+plt.clf()
+plt.scatter(y_test_pred,y_test-y_test_pred)
+plt.title("Predicted vs residuals")
+plt.xlabel("Predicted")
+plt.ylabel("Residuals", labelpad=1.5)
+plt.show()
+
+# Checking Normality of errors
+plt.clf()
+sns.distplot(y_test-y_test_pred)
+plt.title("Histogram of Residuals")
+plt.xlabel("Residuals")
+plt.ylabel("Frequency")
+plt.show()
+
+
+
+print('------------XGBoost Regressor-----------------')
+# Import XGBoost Regressor
+from xgboost import XGBRegressor
+
+#Create a XGBoost Regressor
+reg = XGBRegressor()
+
+# Train the model using the training sets
+reg.fit(X_train, y_train)
+
+# Model prediction on train data
+y_pred = reg.predict(X_train)
+
+# Model Evaluation
+print('R^2:',metrics.r2_score(y_train, y_pred))
+print('Adjusted R^2:',1 - (1-metrics.r2_score(y_train, y_pred))*(len(y_train)-1)/(len(y_train)-X_train.shape[1]-1))
+print('MAE:',metrics.mean_absolute_error(y_train, y_pred))
+print('MSE:',metrics.mean_squared_error(y_train, y_pred))
+print('RMSE:',np.sqrt(metrics.mean_squared_error(y_train, y_pred)))
+
+#Predicting Test data with the model
+y_test_pred = reg.predict(X_test)
+
+# Model Evaluation
+acc_xgb = metrics.r2_score(y_test, y_test_pred)
+mae_xgb = metrics.mean_absolute_error(y_test, y_test_pred)
+rmse_xgb = np.sqrt(metrics.mean_squared_error(y_test, y_test_pred))
+print('R^2:', acc_xgb)
+print('Adjusted R^2:',1 - (1-metrics.r2_score(y_test, y_test_pred))*(len(y_test)-1)/(len(y_test)-X_test.shape[1]-1))
+print('MAE:',mae_xgb)
+print('MSE:',metrics.mean_squared_error(y_test, y_test_pred))
+print('RMSE:',rmse_xgb)
+
+# Visualizing the differences between actual prices and predicted values
+plt.clf()
+plt.scatter(y_test, y_test_pred)
+plt.xlabel("Prices")
+plt.ylabel("Predicted prices", labelpad=1.5)
+plt.title("Prices vs Predicted prices")
+xpoints = ypoints = plt.xlim()
+plt.plot(xpoints, ypoints, linestyle='-', color='r', lw=3, scalex=False, scaley=False)
+
+plt.show()
+
+# Checking residuals
+plt.scatter(y_test_pred,y_test-y_test_pred)
+plt.title("Predicted vs residuals")
+plt.xlabel("Predicted")
+plt.ylabel("Residuals", labelpad=1.5)
+plt.show()
+
+# Checking Normality of errors
+plt.clf()
+sns.distplot(y_test-y_test_pred)
+plt.title("Histogram of Residuals")
+plt.xlabel("Residuals")
+plt.ylabel("Frequency")
+plt.show()
+
+from xgboost import plot_importance
+plt.clf()
+fig,ax = plt.subplots(figsize=(10,10))
+plot_importance(reg,height=0.5,max_num_features=64,ax=ax)
+plt.show()
